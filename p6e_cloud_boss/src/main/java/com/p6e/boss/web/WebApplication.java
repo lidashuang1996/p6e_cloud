27700355e95495a7463e0f74f95b084171fbb05b
lidashuang <1294935733@qq.com>
2020-05-18 22:22:31 +0800

    提交创建的文件夹

---
+++ p6e_cloud_boss/src/main/java/com/p6e/boss/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.p6e.boss.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
